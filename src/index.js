aa4a3c5b3599ce405463791227db0a7a6f99c4b1
jorenbroekema <joren.broekema@gmail.com>
2021-03-09 16:02:35 +0100

    feat: export scaffold functions, avoid wildcard re-export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 export { setCapsule } from './components/setCapsule.js';
 export { generateKey } from './generate-key.js';
-export * from './middleware/middleware.js';
-export * from './plugins/plugins.js';
-export { scaffoldFiles } from './scaffold-files.js';
+export {
+  noCacheMiddleware,
+  changeParticipantUrlMiddleware,
+  jwtMiddleware,
+} from './middleware/middleware.js';
+export {
+  wsPortPlugin,
+  componentReplacersPlugin,
+  followModePlugin,
+  adminUIPlugin,
+  appShellPlugin,
+  queryTimestampModulesPlugin,
+  missingIndexHtmlPlugin,
+} from './plugins/plugins.js';
+export { scaffoldFiles, copyTemplates, scaffold } from './scaffold-files.js';
 export { startServer } from './start-server.js';
